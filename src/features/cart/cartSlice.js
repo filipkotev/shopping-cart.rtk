3fe6a9b918cc52eea4c14ed6409a189838e8ae36
Filip Kotev <filipkotev@yahoo.com>
2024-10-29 16:57:17 +0200

    Reducers - dispatch first action "Clear cart"

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,8 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
+import cartItems from '../../cartItems';
 
 const initialState = {
-  cartItems: [],
-  amount: 0,
+  cartItems,
+  amount: 3,
   total: 0,
   isLoading: true,
 };
@@ -11,10 +12,11 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-
+    clearCart: (state) => {
+      state.cartItems = [];
+    }
   }
 });
 
-console.log(cartSlice);
-
+export const { clearCart } = cartSlice.actions;
 export default cartSlice.reducer;
